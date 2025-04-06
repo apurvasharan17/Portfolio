88767db17f25dd4669b690c7c21ceb5dbc977347
Apurva Sharan <ranjan.apurva2017@gmail.com>
2025-04-07 00:13:20 +0530

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Apurva Sharan | Portfolio",
   description: "Full Stack Developer Portfolio of Apurva Sharan",
+  icons: {
+    icon: "/portfolioIcon.ico",
+  },
 }
 
 export default function RootLayout({
@@ -21,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="flex min-h-screen flex-col">
